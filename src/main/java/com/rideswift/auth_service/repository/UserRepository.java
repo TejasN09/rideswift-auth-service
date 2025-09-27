a9e825b2f7cbcd68ecdac2ff1d695faa53422c9b
Tejas N <tejasn0928@gmail.com>
2025-09-28 01:01:28 +0530

    Missed few commit changes in UserRepository

--- src/main/java/com/rideswift/auth_service/repository/UserRepository.java
+++ src/main/java/com/rideswift/auth_service/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.rideswift.auth_service.repository;
 
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +12,12 @@ import com.rideswift.auth_service.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String lowerCase);
+
+    void lockAccount(String username, Instant plus);
 }
